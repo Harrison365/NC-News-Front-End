ab11864cb312afb5b1e32286846974f07de2bb16
Harrison365 <harrisonroyds@yahoo.co,uk>
2022-04-19 18:44:16 +0100

    test complete

--- src/Tags/Banner.jsx
+++ src/Tags/Banner.jsx
@@ -5,7 +5,7 @@ import "../App.css";
 export default function Banner() {
   return (
     <div id="banner">
-      <h1 id="NcTitle">NC News!</h1>
+      <h1 id="NcTitle">NC News</h1>
       <img id="NcImage" src={ncLogo} />
     </div>
   );
